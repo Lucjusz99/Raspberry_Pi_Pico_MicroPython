040afbd937cabeacbd2452033a753c4c89398774
Lucjusz99 <62251451+Lucjusz99@users.noreply.github.com>
2021-03-09 22:18:05 +0100

    Update main.py

--- Traffic lights/main.py
+++ Traffic lights/main.py
@@ -1,17 +1,19 @@
-from machine import Pin
+from machine import Pin #import potrzebnych funkcji i bibliotek
 from utime import sleep
 import _thread
 
-led=Pin(25, Pin.OUT)
+#przypisanie wszystkich LED do odpowiednich pinów
 led_r=Pin(15, Pin.OUT)
 led_y=Pin(14, Pin.OUT)
 led_g=Pin(13, Pin.OUT)
-button=Pin(16, Pin.IN, Pin.PULL_DOWN)
-buzzer=Pin(12, Pin.OUT)
 led_r_p=Pin(11, Pin.OUT)
 led_y_p=Pin(10, Pin.OUT)
 led_g_p=Pin(9, Pin.OUT)
+#przypisanie buzzera i przycisku
+button=Pin(16, Pin.IN, Pin.PULL_DOWN)
+buzzer=Pin(12, Pin.OUT)
 
+#wygaszanie wszystkich LEDów
 led_r.value(0)
 led_y.value(0)
 led_g.value(0)
@@ -19,20 +21,20 @@ led_r_p.value(0)
 led_y_p.value(0)
 led_g_p.value(0)
 
-global button_pressed
+global button_pressed #definicja zmiennej stanu przycisku
 button_pressed=False
 
-def button_reader_thread():
+def button_reader_thread(): #wątek służący do sprawdzania czy przycisk został wciśnięty
     global button_pressed
     while True:
         if button.value()==1:
-            button_pressed=True
+            button_pressed=True #zmienna przyjmuje wartość true gdy przycisk został wciśnięty
         sleep(0.01)
 _thread.start_new_thread(button_reader_thread, ())
 
-while True:
-    if button_pressed==True:
-        led_g.value(1)
+while True: #nieskończona pętla
+    if button_pressed==True: #jeśli przycisk został wciśnięty:
+        led_g.value(1) #sekwencja LEDów i brzęczka
         led_r_p.value(1)
         sleep(2)
         led_g.value(0)
@@ -59,7 +61,7 @@ while True:
         led_r_p.value(1)
         led_y_p.value(0)
         sleep(2)
-        global button_pressed
+        global button_pressed #zmienna "button_pressed" negowana po sekwencji
         button_pressed=False
-    led_g.value(1)
+    led_g.value(1) #LEDy świecące się gdy przycisk nie został wciśnięty
     led_r_p.value(1)
